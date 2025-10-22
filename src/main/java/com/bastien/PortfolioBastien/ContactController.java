d1fe61383c0c415bb1021e04cee8a506d3903c05
Sanchez <sanchez.bastien34@gmail.com>
2025-10-22 12:29:34 +0200

    fix email

--- src/main/java/com/bastien/PortfolioBastien/ContactController.java
+++ src/main/java/com/bastien/PortfolioBastien/ContactController.java
@@ -1,5 +1,6 @@
 package com.bastien.PortfolioBastien;
 
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.UnsupportedEncodingException;
 
 @Controller
 public class ContactController {
@@ -20,7 +23,7 @@ public class ContactController {
     public String handleContactForm(
             @Valid @ModelAttribute ContactMessage contactMessage,
             BindingResult bindingResult,
-            RedirectAttributes redirectAttributes) {
+            RedirectAttributes redirectAttributes) throws MessagingException, UnsupportedEncodingException {
 
         if (bindingResult.hasErrors()) {
             return "contact";
--- src/main/java/com/bastien/PortfolioBastien/EmailService.java
+++ src/main/java/com/bastien/PortfolioBastien/EmailService.java
@@ -1,29 +1,32 @@
 package com.bastien.PortfolioBastien;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Autowired private JavaMailSender mailSender;
 
-    public void sendContactEmail(String fromName, String fromEmail, String messageContent) {
-        SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("sanchez.bastien34@gmail.com"); // expéditeur validé sur Brevo
-        message.setTo("sanchez.bastien34@gmail.com");   // destinataire
-
-        message.setSubject("Nouveau message du portfolio - " + fromName);
-        message.setText("Nom : " + fromName + "\nEmail : " + fromEmail + "\n\nMessage :\n" + messageContent);
-
-        System.out.println(">>> Tentative d'envoi du mail via Brevo...");
-
-        mailSender.send(message);
-
-        System.out.println(">>> Mail envoyé avec succès !");
+    public void sendContactEmail(String fromName, String fromEmail, String messageContent) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage mime = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mime, "UTF-8");
+        helper.setFrom("sanchez.bastien34@gmail.com");
+        helper.setTo("sanchez.bastien34@gmail.com");
+        helper.setReplyTo(fromEmail, fromName);
+        helper.setSubject("Nouveau message du portfolio - " + fromName);
+        helper.setText(
+                "Nom : " + fromName + "\nEmail : " + fromEmail + "\n\nMessage :\n" + messageContent,
+                false
+        );
+        mailSender.send(mime);
     }
 }
